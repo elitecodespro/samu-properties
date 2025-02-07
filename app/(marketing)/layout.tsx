b5c9b87824f4b898120f4b2caa9cab42ac965adf
secondlife009 <secondlife986@gmail.com>
2025-02-07 18:56:24 +0400

    Fixed the undefined window

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,11 +1,23 @@
+"use client"
+
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import { useEffect, useState } from "react";
 
 export default function MarketingLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  useEffect(() =>{
+    setIsLoaded(true)
+  }, [isLoaded])
+
+  if (!isLoaded) {
+    return <h1>LOADING...</h1>
+  }
   return (
     <>
         <Header />
